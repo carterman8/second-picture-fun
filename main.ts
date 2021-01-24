718c103b43630296e1171579443c212ec516ae5b
carterman8 <76662733+carterman8@users.noreply.github.com>
2021-01-24 18:19:18 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+function Anything () {
+    basic.showString("GO BILLS")
+}
+input.onButtonPressed(Button.A, function () {
+    Anything()
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    Anything()
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    Anything()
+})
+input.onButtonPressed(Button.AB, function () {
+    Anything()
+})
+input.onButtonPressed(Button.B, function () {
+    Anything()
+})
+input.onGesture(Gesture.Shake, function () {
+    Anything()
+})
+input.onGesture(Gesture.TiltRight, function () {
+    Anything()
+})
+input.onGesture(Gesture.LogoDown, function () {
+    Anything()
+})
